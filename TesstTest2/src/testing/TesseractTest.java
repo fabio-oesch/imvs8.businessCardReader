2673bdf536ca115546a733a44ae1f8458b6dcd5d
Jonas Schwammberger <hooovercat@gmail.com>
2013-12-28 12:23:59 +0100

    implemented fmeasure for tesseracttest

--- TesstTest2/src/testing/TesseractTest.java
+++ TesstTest2/src/testing/TesseractTest.java
@@ -24,24 +24,26 @@ public class TesseractTest {
 		testImagesFolder = new File("C:\\Users\\Jon\\FHNW\\IP5\\testdata\\tesseract-testdata\\testimages");
 		testFile = new File("C:\\Users\\Jon\\FHNW\\IP5\\testdata\\tesseract-testdata\\solutionText.txt");
 
-		BufferedWriter out = new BufferedWriter(new FileWriter("C:\\Users\\Jon\\FHNW\\IP5\\testdata\\tesseract-testdata\\log.csv"));
+		FileWriter fileStream = new FileWriter("C:\\Users\\Jon\\FHNW\\IP5\\testdata\\tesseract-testdata\\log.csv");
 
+		BufferedWriter out = new BufferedWriter(fileStream);
 		BufferedReader r = new BufferedReader(new FileReader(testFile));
 		solution = r.readLine();
-
+		System.out.println(solution);
 		OCREngine engine = new OCREngine();
 		diff_match_patch diffEngine = new diff_match_patch();
 
-		out.append("Filename;#inserted characters;#deleted characters;inserted String;deleted String\n");
+		out.append("Filename;precision;recall;f-measure;inserted String;deleted String\n");
 		//test
 		for (File f : testImagesFolder.listFiles()) {
 			int inserted = 0;
 			int deleted = 0;
+			int correct = 0;
 			StringBuilder insertedStr = new StringBuilder();
 			StringBuilder deletedStr = new StringBuilder();
 
 			String result = buildResultString(engine.analyzeImage(f));
-
+			System.out.println(result);
 			List<Diff> differences = diffEngine.diff_main(solution, result);
 			for (Diff d : differences) {
 				if (d.operation == Operation.DELETE) {
@@ -51,15 +53,22 @@ public class TesseractTest {
 					deletedStr.append(" | ");
 
 				} else if (d.operation == Operation.INSERT) {
-					//inserted, changed = false positive
+					//inserted = false positive
 					inserted += d.text.length();
 					insertedStr.append(d.text);
 					insertedStr.append(" | ");
+				} else {
+					correct += d.text.length();
+					//System.out.println(d.text + "changed");
 				}
 			}
+			double precision = correct / (double) (correct + inserted);
+			double recall = correct / (double) (correct + deleted);
+			double fmeasure = 2 * precision * recall / (precision + recall);
 			out.append(f.getName() + " ;");
-			out.append(inserted + " ;");
-			out.append(deleted + " ;");
+			out.append(String.format("%.3f", precision) + " ;");
+			out.append(String.format("%.3f", recall) + " ;");
+			out.append(String.format("%.3f", fmeasure) + " ;");
 			out.append(insertedStr + " ;");
 			out.append(deletedStr + " \n");
 
