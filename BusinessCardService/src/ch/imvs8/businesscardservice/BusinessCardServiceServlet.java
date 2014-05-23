1329e8dbe5533841473a18e184b40c7ecfdfcad9
Jonas Schwammberger <hooovercat@gmail.com>
2014-05-23 13:21:52 +0200

    ok, service is finally working

--- BusinessCardService/src/ch/imvs8/businesscardservice/BusinessCardServiceServlet.java
+++ BusinessCardService/src/ch/imvs8/businesscardservice/BusinessCardServiceServlet.java
@@ -1,75 +1,90 @@
 package ch.imvs8.businesscardservice;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
-import org.apache.tomcat.util.http.fileupload.FileItem;
-import org.apache.tomcat.util.http.fileupload.FileItemIterator;
-import org.apache.tomcat.util.http.fileupload.disk.DiskFileItemFactory;
-import org.apache.tomcat.util.http.fileupload.servlet.ServletFileUpload;
-
-@WebServlet("/scanner")
+@WebServlet(name = "BusinessCardService", urlPatterns = { "/scanner" })
+@MultipartConfig
 public class BusinessCardServiceServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	private static final int maxFileSize = 50 * 1024;
 	private static final int maxMemSize = 4 * 1024;
-	private static final String saveFolder = "/";
 
-	private final String repoFolder;
-	private DiskFileItemFactory factory;
+	private String uploadedFolder;
 
 	public BusinessCardServiceServlet() {
-		final String os = System.getProperty("os.name");
 
-		if (os.contains("Windows"))
-			repoFolder = "C:\\temp";
-		else
-			repoFolder = "/tmp";
+	}
 
-		factory = new DiskFileItemFactory();
-		factory.setSizeThreshold(maxMemSize);
-		// Location to save data that is larger than maxMemSize.
-		factory.setRepository(new File(this.repoFolder));
+	@Override
+	public void init(ServletConfig servletConfig) throws ServletException {
+		this.uploadedFolder = servletConfig.getInitParameter("uploadedFolder");
 	}
 
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("get");
 		request.getRequestDispatcher("BusinessCardService/start.html").forward(request, response);
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Post");
 		response.setContentType("text/html");
-
 		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Business Card Service</title>");
-		out.println("</head>");
-		out.println("<body>");
-		if (!ServletFileUpload.isMultipartContent(request)) {
-			out.println("<p>Nothing uploaded</p>");
-		} else {
-			// Create a new file upload handler
+		DateFormat dateFormat = new SimpleDateFormat("yyyy.MM.dd_HH:mm:ss");
+		Date date = new Date(System.currentTimeMillis());
 
-			ServletFileUpload upload = new ServletFileUpload(factory);
+		OutputStream output = null;
+		InputStream filecontent = null;
 
-			// maximum file size to be uploaded.
-			upload.setSizeMax(maxFileSize);
-			System.out.println("hello");
-			File f = this.saveFile(upload, request);
-			//businesscardreader call
-			this.printUserForm(out);
+		try {
+			final Part filePart = request.getPart("file");
+			final String fileName = getFileName(filePart);
+
+			//create parent folder
+			File parentFolder = new File(uploadedFolder + File.separator + dateFormat.format(date));
+			if (!parentFolder.exists())
+				parentFolder.mkdirs();
+
+			File image = new File(parentFolder.getAbsolutePath() + File.separator + fileName);
+			//write file
+			output = new FileOutputStream(image);
+			filecontent = filePart.getInputStream();
+			int read = 0;
+			final byte[] bytes = new byte[1024];
+
+			while ((read = filecontent.read(bytes)) != -1) {
+				output.write(bytes, 0, read);
+			}
+
+			output.close();
+			filecontent.close();
+
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Business Card Service</title>");
+			out.println("</head>");
+			out.println("<body>");
+
+			this.scanAndPrintResults(image, out);
+		} catch (Exception e) {
+			out.println("<p>Error while uploading Image.</p>");
 		}
 
 		out.println("</body>");
@@ -82,51 +97,22 @@ public class BusinessCardServiceServlet extends HttpServlet {
 		request.getRequestDispatcher("BusinessCardService/thankyou.html").forward(request, response);
 	}
 
-	/**
-	 * Saves a file by the user
-	 * 
-	 * @param upload
-	 * @param request
-	 * @return File object
-	 */
-	private File saveFile(ServletFileUpload upload, HttpServletRequest request) {
-		File file = null;
-		try {
-			System.out.println("bla");
-			FileItemIterator it = upload.getItemIterator(request);
-			/*
-			 * Map<String, List<FileItem>> stuff =
-			 * upload.parseParameterMap(request);
-			 * 
-			 * Iterator<List<FileItem>> it = stuff.values().iterator();
-			 */
-
-			while (it.hasNext()) {
-				FileItem item = (FileItem) it.next();
-				if (!item.isFormField()) {
-					String fileName = item.getName();
-					String contentType = item.getContentType();
-					boolean isInMemory = item.isInMemory();
-					long sizeInBytes = item.getSize();
-					file = new File(saveFolder + fileName);
-					item.write(file);
-					System.out.println(file);
-					return file;
-				}
+	private String getFileName(final Part part) {
+		final String partHeader = part.getHeader("content-disposition");
+		for (String content : part.getHeader("content-disposition").split(";")) {
+			if (content.trim().startsWith("filename")) {
+				return content.substring(content.indexOf('=') + 1).trim().replace("\"", "");
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
-
 		return null;
 	}
 
-	private void printUserForm(PrintWriter out) {
+	private void scanAndPrintResults(File image, PrintWriter out) {
+
 		out.println("<form name=\"user-solution\" method=\"put\" action=\"/BusinessCardService/scanner\">");
 		//color STYLE="color: #FFFFFF; font-family: Verdana; font-weight: bold; font-size: 12px; background-color: #72A4D2;"
 
 		out.println("");
 		out.println("<input type=\"submit\" value=\"Submit\">");
-		out.println("</form>");
 	}
 }
