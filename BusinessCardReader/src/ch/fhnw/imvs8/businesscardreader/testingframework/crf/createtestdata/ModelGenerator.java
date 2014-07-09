e0b334a2c46760b56fa9a9494dd6addf2c7dc8a2
helldevastator <hooovercat@gmail.com>
2014-07-09 14:19:55 +0200

    stuff

--- BusinessCardReader/src/ch/fhnw/imvs8/businesscardreader/testingframework/crf/createtestdata/ModelGenerator.java
+++ BusinessCardReader/src/ch/fhnw/imvs8/businesscardreader/testingframework/crf/createtestdata/ModelGenerator.java
@@ -16,6 +16,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import ch.fhnw.imvs8.businesscardreader.ner.FeatureCreator;
 import ch.fhnw.imvs8.businesscardreader.ner.LabeledWord;
@@ -74,6 +75,8 @@ public class ModelGenerator {
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
+		Arrays.sort(files);
+		
 		for (File file : files) {
 
 			// does not create a new file
